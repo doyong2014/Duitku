f2389233edb960d17c63d90161a8b05d4d1a2b0c
timur <timur@chakratechnology.com>
2017-03-27 19:18:20 +0700

    add root url for configuration ease

--- app/src/main/java/com/mi1/duitku/Common/Constant.java
+++ app/src/main/java/com/mi1/duitku/Common/Constant.java
@@ -2,27 +2,31 @@ package com.mi1.duitku.Common;
 
 public class Constant {
 
-	public static final String LOGIN_PAGE = "http://sandbox.duitku.com/webapi/api/loginduitku";
-	public static final String SIGNUP_PAGE = "http://sandbox.duitku.com/webapi/api/user/registermi1";
-	public static final String RECOVERY_PASSWORD_PAGE = "http://sandbox.duitku.com/webapi/api/user/forgotpasswordduitku";
-	public static final String VERIFY_CODE_PAGE = "http://sandbox.duitku.com/webapi/api/user/verifyotpphoneduitku";
-	public static final String RESUBMIT_CODE_PAGE = "http://sandbox.duitku.com/webapi/api/user/resendotpphoneduitku";
+	//production server
+	//private static final String root_url = "https://passport.duitku.com/";
+	//sandbox server
+	private static final String root_url = "http://sandbox.duitku.com/";
+	public static final String LOGIN_PAGE = root_url + "webapi/api/loginduitku";
+	public static final String SIGNUP_PAGE = root_url + "webapi/api/user/registermi1";
+	public static final String RECOVERY_PASSWORD_PAGE = root_url + "webapi/api/user/forgotpasswordduitku";
+	public static final String VERIFY_CODE_PAGE = root_url + "webapi/api/user/verifyotpphoneduitku";
+	public static final String RESUBMIT_CODE_PAGE = root_url + "webapi/api/user/resendotpphoneduitku";
 	public static final String NEWS_PAGE = "http://renemo.com/komunitas/api/get_category_posts/?category_slug=DUITKU";
 	public static final String PROMOV_PAGE = "http://renemo.com/komunitas/api/get_category_posts/?category_slug=PROMO";
 	public static final String EVENTS_PAGE = "http://renemo.com/komunitas/api/get_category_posts/?category_slug=EVENT";
-	public static final String PRODUCT_LIST_PAGE = "http://sandbox.duitku.com/webapi/api/ppob/listv2";
-	public static final String INQUIRY_BILL_PAGE = "http://sandbox.duitku.com/webapi/api/ppob/inquiry";
-	public static final String PAYMENT_BILL_PAGE = "http://sandbox.duitku.com/webapi/api/ppob/payment";
-	public static final String PURCHASE_BILL_PAGE = "http://sandbox.duitku.com/webapi/api/ppob/voucher";
-	public static final String BANK_LIST_PAGE = "http://sandbox.duitku.com/webapi/api/topup/paymentmethod";
-	public static final String TOPUP_INQUIRY_PAGE = "http://sandbox.duitku.com/webapi/api/topup/request";
-	public static final String CASH_IN_PAGE = "http://sandbox.duitku.com/webapi/api/history/historyCashInPaginationDuitku";
-	public static final String CASH_OUT_PAGE = "http://sandbox.duitku.com/webapi/api/history/historyCashOutPaginationDuitku";
-	public static final String GET_BALANCE_PAGE = "http://sandbox.duitku.com/webapi/api/user/balanceduitku";
-	public static final String GET_PROFILE_PAGE = "http://sandbox.duitku.com/webapi/api/user/";
-	public static final String UPDATE_PROFILE_PAGE = "http://sandbox.duitku.com/webapi/api/user/updateprofileduitku";
-	public static final String UPLOAD_IMAGE_PAGE = "http://sandbox.duitku.com/webapi/api/user/uploadprofile/";
-	public static final String CHANGE_PASSWORD_PAGE = "http://sandbox.duitku.com/webapi/api/user/changepasswordduitku";
+	public static final String PRODUCT_LIST_PAGE = root_url + "webapi/api/ppob/listv2";
+	public static final String INQUIRY_BILL_PAGE = root_url +  "webapi/api/ppob/inquiry";
+	public static final String PAYMENT_BILL_PAGE = root_url + "webapi/api/ppob/payment";
+	public static final String PURCHASE_BILL_PAGE = root_url + "webapi/api/ppob/voucher";
+	public static final String BANK_LIST_PAGE = root_url + "webapi/api/topup/paymentmethod";
+	public static final String TOPUP_INQUIRY_PAGE = root_url + "webapi/api/topup/request";
+	public static final String CASH_IN_PAGE = root_url + "webapi/api/history/historyCashInPaginationDuitku";
+	public static final String CASH_OUT_PAGE = root_url + "webapi/api/history/historyCashOutPaginationDuitku";
+	public static final String GET_BALANCE_PAGE = root_url + "webapi/api/user/balanceduitku";
+	public static final String GET_PROFILE_PAGE = root_url + "webapi/api/user/";
+	public static final String UPDATE_PROFILE_PAGE = root_url + "webapi/api/user/updateprofileduitku";
+	public static final String UPLOAD_IMAGE_PAGE = root_url + "webapi/api/user/uploadprofile/";
+	public static final String CHANGE_PASSWORD_PAGE = root_url + "webapi/api/user/changepasswordduitku";
 	public static final String SEARCH_NEWS_PAGE = "http://renemo.com/komunitas/api/get_search_results/?search=";
 	
 	public static final String JSON_STATUS_MESSAGE = "statusMessage";
